790469a902bc0f6b95a4a0974c4fd77534a8aace
Andrea Torelli <gioran326@protonmail.com>
2023-03-13 12:24:40 +0100

    Milestone 1 Finita

---
+++ js/main.js
@@ -0,0 +1,146 @@
+let icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const containerCardsDom = document.getElementById("containerCards");
+const inputDom = document.getElementById("input");
+
+icons.forEach((oggetto) => {
+
+   creaCarta(containerCardsDom,oggetto);
+
+});
+
+
+
+inputDom.addEventListener("change", () => {
+
+   //qualcosa...
+   containerCardsDom.innerHTML="";
+
+});
+
+
+function creaCarta(destinazione,oggetto) {
+
+   let carta = `  <div class="card m-3" style="width: 18rem;">
+                     <div class="card-body p-4 text-center">
+                        <h2 class="card-title"><i class="fa-solid ${oggetto.prefix}${oggetto.name}"></i></h2>
+                        <h3 class="card-text">${oggetto.name}</h3>
+                        </div>
+                     </div>
+                  </div>` 
+
+   destinazione.innerHTML += carta;
+}
